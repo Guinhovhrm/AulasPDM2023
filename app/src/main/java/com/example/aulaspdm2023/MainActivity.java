ffb254bb6415916a323fd980a681d01111c4a138
GuinhoIFPR <macharetevic@gmail.com>
2023-03-02 12:23:02 -0300

    Adicionado ações nos botoes para Sala, Bath, Room.

--- app/src/main/java/com/example/aulaspdm2023/MainActivity.java
+++ app/src/main/java/com/example/aulaspdm2023/MainActivity.java
@@ -2,13 +2,46 @@ package com.example.aulaspdm2023;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnSala;
+    Button btnCozinha;
+    Button btnBath;
+    Button btnRoom;
+    Button btnRoom2;
+    Button btnOffice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSala = findViewById(R.id.btnSala);
+        btnSala.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View view){
+                Intent in = new Intent(MainActivity.this, activitySala.class);
+            }
+        });
+
+        btnBath = findViewById(R.id.btnBath);
+        btnBath.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View view){
+                Intent in = new Intent(MainActivity.this, activityBath.class);
+            }
+        });
+        btnCozinha = findViewById(R.id.btnCozinha);
+
+        btnRoom = findViewById(R.id.btnRoom);
+        btnRoom.setOnClickListener(new View.OnClickListener(){
+            public void onClick (View view){
+                Intent in = new Intent(MainActivity.this, activityRoom.class);
+            }
+        });
+
+        btnRoom2 = findViewById(R.id.btnRoom2);
+        btnOffice = findViewById(R.id.btnOffice);
+
     }
 }
---
+++ app/src/main/java/com/example/aulaspdm2023/activitySala.java
@@ -0,0 +1,14 @@
+package com.example.aulaspdm2023;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activitySala extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sala);
+    }
+}
