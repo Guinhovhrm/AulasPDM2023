e2067125344112d64d3f741be764430a7a342702
Guinhovhrm <macharetevic@gmail.com>
2023-03-16 12:30:05 -0300

    Calculadora simples completa

--- app/src/main/java/com/example/aulaspdm2023/activityCalculadoraSimples.java
+++ app/src/main/java/com/example/aulaspdm2023/activityCalculadoraSimples.java
@@ -11,8 +11,8 @@ import android.widget.EditText;
 public class activityCalculadoraSimples extends AppCompatActivity {
     Button btnVoltar;
     Button btnCalcular;
-    Button btnMais;
-    EditText edN1, edN2;
+    Button btnMais, btnMenos, btnVezes, btnDiv;
+    EditText edN1, edN2, edN3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +20,7 @@ public class activityCalculadoraSimples extends AppCompatActivity {
         setContentView(R.layout.activity_calculadorasimples);
         edN1 = findViewById(R.id.edN1);
         edN2 = findViewById(R.id.edN2);
+        edN3 = findViewById(R.id.edN3);
         btnVoltar = findViewById(R.id.btnVoltar);
         btnVoltar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,12 +35,46 @@ public class activityCalculadoraSimples extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 double n1 = Double.parseDouble(edN1.getText().toString());
-                double n2 = Double.parseDouble(edN1.getText().toString());
+                double n2 = Double.parseDouble(edN2.getText().toString());
                 double soma = n1 + n2;
-
-
+                String somaSt = String.valueOf(soma);
+                edN3.setText(somaSt);
             }
         });
+        btnMenos = findViewById(R.id.btnMenos);
+        btnMenos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double n1 = Double.parseDouble(edN1.getText().toString());
+                double n2 = Double.parseDouble(edN2.getText().toString());
+                double sub = n1 - n2;
+                String subSt = String.valueOf(sub);
+                edN3.setText(subSt);
+            }
+        });
+        btnVezes = findViewById(R.id.btnVezes);
+        btnVezes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double n1 = Double.parseDouble(edN1.getText().toString());
+                double n2 = Double.parseDouble(edN2.getText().toString());
+                double mult = n1 * n2;
+                String multSt = String.valueOf(mult);
+                edN3.setText(multSt);
+            }
+        });
+        btnDiv = findViewById(R.id.btnDiv);
+        btnDiv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double n1 = Double.parseDouble(edN1.getText().toString());
+                double n2 = Double.parseDouble(edN2.getText().toString());
+                double div = n1 / n2;
+                String divSt = String.valueOf(div);
+                edN3.setText(divSt);
+            }
+        });
+
 
 
 
